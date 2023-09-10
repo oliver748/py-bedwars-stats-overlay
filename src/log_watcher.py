d9292ab3fa69b4ab058f85e28408cc7c7a0a879c
Oliver <113297002+oliver748@users.noreply.github.com>
2023-09-10 12:07:59 +0200

    Update log_watcher.py

--- src/log_watcher.py
+++ src/log_watcher.py
@@ -5,7 +5,8 @@ class LogWatcher(QThread):
     new_player_signal = pyqtSignal(str)
 
     def run(self):
-        # log file is typically in C:/Users/PC_USERNAME/.lunarclient/logs/game/
+        # if you use lunar client: log file is typically in C:/Users/PC_USERNAME/.lunarclient/logs/game/
+        # if you use normal minecraft it should be in .minecraft/logs or something
         with open('POINT_TO_THE_LOG_FILE', 'r', errors='ignore') as f:
             f.seek(0, 2)  # Go to the end of the file
             while True:
