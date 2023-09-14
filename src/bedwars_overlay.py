618ad7daf21270e81af88e58c9faffaee8705d60
Oliver <113297002+oliver748@users.noreply.github.com>
2023-09-14 21:22:22 +0200

    moved settings gui out, colors for fkdr (easy to add for other stuff)

--- src/bedwars_overlay.py
+++ src/bedwars_overlay.py
@@ -1,17 +1,24 @@
 from PyQt6.QtCore import Qt, QThreadPool, QRunnable, pyqtSignal, pyqtSlot, QObject, QSize
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView, QLabel, QLineEdit, QHBoxLayout, QPushButton, QDialog
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QColor
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView, QHBoxLayout, QPushButton
+
+from src.settings_window import SettingsWindow
 from src.log_watcher import LogWatcher
 from src.stats_fetcher import StatsFetcher
 import re
 
+
+
 class BedwarsOverlay(QWidget):
+    BUTTON_STYLE = "background-color: #292929; color: white;"
+    
     def __init__(self, api_key):
         super().__init__()
 
         self.setWindowTitle("Bedwars Overlay v1.0")
-        
-        self.own_player_name = "YOUR_OWN_NAME_HERE"
+        self.own_player_name = ""
+
+        self.show_own_name = True  # By default, show the own name in the table
 
         self.stats_fetcher = StatsFetcher()
         self.stats_fetcher.update_api_key(api_key)
@@ -19,13 +26,28 @@ class BedwarsOverlay(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
+        self.setup_top_layout()
+        self.setup_table()
+        
+        self.log_watcher = LogWatcher()
+        self.log_watcher.new_player_signal.connect(self.update_table)
+        self.log_watcher.start()
+
+        self.thread_pool = QThreadPool()
+        self.players_in_table = []
+
+        self.settings_window = SettingsWindow(api_key, self.log_watcher)
+        self.settings_window.player_name_changed_signal.connect(self.update_own_player_name)  # new line
+        self.settings_window.show_own_name_checkbox_signal.connect(self.update_show_own_name)  # new line
+
+        
+    def setup_top_layout(self):
         settings_button = QPushButton()
         settings_button.setIcon(QIcon("cogwheel.png"))
         settings_button.setIconSize(QSize(24, 24))
         settings_button.setFixedSize(32, 32)
-        settings_button.setStyleSheet("background-color: #292929; color: white;")
+        settings_button.setStyleSheet(self.BUTTON_STYLE)
         settings_button.clicked.connect(self.open_settings)
-
         
         top_layout = QHBoxLayout()
         top_layout.addWidget(settings_button, alignment=Qt.AlignmentFlag.AlignLeft)
@@ -33,43 +55,39 @@ class BedwarsOverlay(QWidget):
         self.layout.addLayout(top_layout)
         self.layout.addSpacing(5)
 
-
-        self.labels = ["Username", "FKs", "FDs", "FKDR", "Wins", "W/L", "Beds Broken"]
-
+    def setup_table(self):
+        labels = ["Username", "FKs", "FDs", "FKDR", "Wins", "W/L", "Beds Broken"]
         self.table = QTableWidget()
-        self.table.setColumnCount(len(self.labels))
-        self.table.setHorizontalHeaderLabels(self.labels)
-        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Interactive)        
+        self.table.setColumnCount(len(labels))
+        self.table.setHorizontalHeaderLabels(labels)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Interactive)
         self.table.setSelectionMode(QTableWidget.SelectionMode.NoSelection)
         self.table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
-        self.table.setStyleSheet("background-color: #292929; color: white;")
+        self.table.setStyleSheet(self.BUTTON_STYLE)
         self.layout.addWidget(self.table)
-
-        self.log_watcher = LogWatcher()
-        self.log_watcher.new_player_signal.connect(self.update_table)
-        self.log_watcher.start()
-
-        self.thread_pool = QThreadPool()
-
-        self.players_in_table = []
-
-        self.settings_window = SettingsWindow(api_key)
-
+    
     def open_settings(self):
         self.settings_window.show()
+
+    def update_show_own_name(self, state):
+        self.show_own_name = bool(state)
+
+    def update_own_player_name(self, new_name):
+        print(f"Updating own player name to {new_name}")
+        self.own_player_name = new_name
 
     def update_api_key(self, new_api_key):
         print(f"Updating API key to {new_api_key}")
         self.stats_fetcher.update_api_key(new_api_key)
 
     def update_table(self, log_line):
-        join_pattern = r'\[info\]  \[\d{2}:\d{2}:\d{2}\] \[Client thread/INFO\]: \[CHAT\] ([\w]+) has joined \(.*\)'
+        join_pattern = r'\[\d{2}:\d{2}:\d{2}\] \[.*? thread/INFO\]: \[CHAT\] ([\w]+) has joined( \(.*\))?'
         join_match = re.search(join_pattern, log_line)
         
-        quit_pattern = r'\[info\]  \[\d{2}:\d{2}:\d{2}\] \[Client thread/INFO\]: \[CHAT\] ([\w]+) has quit!'
+        quit_pattern = r'\[\d{2}:\d{2}:\d{2}\] \[.*? thread/INFO\]: \[CHAT\] ([\w]+) has quit!'
         quit_match = re.search(quit_pattern, log_line)
-
-        who_pattern = r'\[info\]  \[\d{2}:\d{2}:\d{2}\] \[Client thread/INFO\]: \[CHAT\] ONLINE: ((?:[\w]+, )+[\w]+)'
+        
+        who_pattern = r'\[\d{2}:\d{2}:\d{2}\] \[.*? thread/INFO\]: \[CHAT\] ONLINE: ((?:[\w]+, )*[\w]+)'
         who_match = re.search(who_pattern, log_line)
 
         if join_match:
@@ -79,8 +97,7 @@ class BedwarsOverlay(QWidget):
                 print("Resetting table...")
                 self.table.setRowCount(0)  # Remove all rows
                 self.players_in_table = []  # Reset list
-                return
-            
+
             self.add_new_player(player_name)
 
         elif quit_match:
@@ -93,9 +110,10 @@ class BedwarsOverlay(QWidget):
             for player_name in player_names:
                 self.add_new_player(player_name)
 
-
     def add_new_player(self, name):
         if name in self.players_in_table:
+            return
+        if name == self.own_player_name and not self.show_own_name:
             return
         
         print(f"Adding {name} to table")
@@ -122,43 +140,44 @@ class BedwarsOverlay(QWidget):
             if self.table.item(row, 0).text() == name:
                 self.table.removeRow(row)
                 break
+    
+
+    def find_color(self, fkdr):
+        if fkdr == '?':
+            return QColor("#FFFFFF")  # White
+        elif fkdr <= 0.1:
+            return QColor("#878787")
+        elif fkdr <= 0.5:
+            return QColor("#ADADAD")
+        elif fkdr <= 1:
+            return QColor("#FFC7BA")
+        elif fkdr <= 2:
+            return QColor("#FFA384")
+        elif fkdr <= 3:
+            return QColor("#FF7A4D")
+        elif fkdr <= 4:
+            return QColor("#FF5500")
+        elif fkdr <= 5:
+            return QColor("#E64D00")
+        elif fkdr <= 6:
+            return QColor("#CC4400")
+        elif fkdr > 6:
+            return QColor("#F07F65")
 
     @pyqtSlot(int, list)
     def display_stats(self, row_position, stats):
         for i, stat in enumerate(stats):
+            print(i, stat)
             if i == 0:
                 item = QTableWidgetItem(str(stat))
             else:
                 item = NumericTableWidgetItem(str(stat))
 
             item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsSelectable)
+
             self.table.setItem(row_position, i, item)
-
-
-class SettingsWindow(QDialog):
-    def __init__(self, api_key):
-        super().__init__()
-
-        self.setWindowTitle("Settings")
-        self.setFixedWidth(350)
-        self.setFixedHeight(50)
-
-        layout = QVBoxLayout()
-
-        api_label = QLabel("API Key:")
-        self.api_input = QLineEdit()
-        self.api_input.setText(api_key)
-        self.api_input.textChanged.connect(self.update_api_key)
-
-        api_layout = QHBoxLayout()
-        api_layout.addWidget(api_label)
-        api_layout.addWidget(self.api_input)
-
-        layout.addLayout(api_layout)
-        self.setLayout(layout)
-
-    def update_api_key(self, new_api_key):
-        print(f"Updating API key to {new_api_key}")
+            if i == 3:
+                self.table.item(row_position, i).setForeground(self.find_color(stat))
 
 
 class WorkerSignals(QObject):
